54156ceee9724e64a98111435656d63a44de16d9
chbaltazar <52985894+chbaltazar@users.noreply.github.com>
2022-06-10 22:02:09 -0500

    Adding input text to get fruit information

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
+# New section to display fruityvice API response
 streamlit.header('Fruityvice Fruit Advice!')
+fruit_choise = streamlit.text_input('What fruit would you like information about', 'Kiwi')
+streamlit.write('The user entered ', fruit_choise)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choise)
 
 # Take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
