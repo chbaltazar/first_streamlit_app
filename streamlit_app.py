9cac7a846f5ef00d996bd4daa5e98bf72ddd2bde
chbaltazar <52985894+chbaltazar@users.noreply.github.com>
2022-06-14 21:43:51 -0500

    Adding validation to get fruit information

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,16 +26,17 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice API response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choise = streamlit.text_input('What fruit would you like information about', 'Kiwi')
-streamlit.write('The user entered ', fruit_choise)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choise)
-
-# Take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-# Output it the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choise = streamlit.text_input('What fruit would you like information about')
+  
+  if not fruit_choise:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choise)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 # dont run anything past here while we trobleshoot
 streamlit.stop()
